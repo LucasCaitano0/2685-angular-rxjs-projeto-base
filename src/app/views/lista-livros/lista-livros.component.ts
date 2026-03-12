f215203b82565f31511fb2ea04ac7c2b612d3e0c
Lucas Caitaino Barreto <lucas.caitano.barreto@gmail.com>
2026-03-12 16:27:57 -0300

    iniciando o observable

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
@@ -8,8 +9,20 @@ import { Component } from '@angular/core';
 export class ListaLivrosComponent {
 
   listaLivros: [];
+  campoBusca: string = ''
 
-  constructor() { }
+  constructor(private service: LivroService) { }
+
+  buscarLivros(){
+    this.service.buscar(this.campoBusca).subscribe({
+      next: retornoAPI => console.log(retornoAPI),
+      error: erro => console.error(erro),
+      complete: () => console.log("Observable completado")
+    }
+      
+
+  )
+  }
 
 }
 
