683dd851af9066342dd6030f0884bcc5be3dda76
Lucas Caitaino Barreto <lucas.caitano.barreto@gmail.com>
2026-03-23 11:02:01 -0300

    renderizando

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,3 +1,4 @@
+import { Livro } from './../../models/interfaces';
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { LivroService } from 'src/app/service/livro.service';
@@ -9,20 +10,40 @@ import { LivroService } from 'src/app/service/livro.service';
 })
 export class ListaLivrosComponent implements OnDestroy{
 
-  listaLivros: [];
+  listaLivros: Livro[];
   campoBusca: string = ''
   subscription: Subscription
+  livro: Livro
 
   constructor(private service: LivroService) { }
 
   buscarLivros(){
     this.subscription = this.service.buscar(this.campoBusca).subscribe({
-      next: retornoAPI => console.log(),
+      next: (items) => {
+        this.listaLivros = this.livrosResultadoaParaLivros(items)
+      },
       error: erro => console.error(erro),
       complete: () => console.log("Observable completado")
     }
     
     )
+  }
+
+  livrosResultadoaParaLivros(items): Livro[]{
+    const livros: Livro[] = []
+
+    items.forEach(item => {
+      livros.push(this.livro = {
+        title: item.volumeInfo?.title,
+        authors: item.volumeInfo?.authors,
+        publisher: item.volumeInfo?.publisher,
+        publishedDate: item.volumeInfo?.publishedDate,
+        description: item.volumeInfo?.description,
+        previewLink: item.volumeInfo?.previewLink,
+        thumbnail: item.volumeInfo?.imageLinks?.thumbnail
+      })
+    })
+    return livros
   }
 
   ngOnDestroy() {
